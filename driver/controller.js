0336232da7eda989a47e8b21c44caa4072288b09
Cédric Legendre <albundy1385@gmail.com>
2018-01-04 13:57:00 +0100

    Add more caching and returns cached value instead of API calls. The polling delay is now every second

--- driver/controller.js
+++ driver/controller.js
@@ -8,8 +8,9 @@ class Controller {
         this.player = player;
         this._mappings = {};
         this._settings = {
-            pollingDelay: 5 * 1000 // 5 seconds
+            pollingDelay: 1 // 1 second
         }
+        this._status = {};
         this._cache = {};
     }
 
@@ -81,10 +82,12 @@ class Controller {
 
     addNavigationButtons(){
         return this.addButton(['SKIP BACKWARD','CHANNEL DOWN','CURSOR LEFT'], () => this.player.playIndex(-1, cb => {
-                this.updateCacheFromMeta();
+                this.updateStatus();
+                this.updateRemoteMeta();
             } ) )
             .addButton(['SKIP FORWARD','CHANNEL UP', 'CURSOR RIGHT'], () => this.player.playIndex(1, cb => {
-                this.updateCacheFromMeta();
+                this.updateStatus();
+                this.updateRemoteMeta();
             } ) );
     }
 
@@ -100,19 +103,11 @@ class Controller {
             range:[0,100], 
             unit: '%'
         }, {
-            setter: (deviceId,duration) => { 
-                this.player.getStatus( ({result}) => {
-                    const durationInTime = Math.round( (duration * result.duration)/100 );
-                    this.player.seek( durationInTime );
-                } );
+            setter: (deviceId,value) => { 
+                const durationInSeconds =  (value * this._status.duration)/100;
+                this.player.seek( durationInSeconds );
             },
-            getter: () => {
-                return new Promise( (resolve, reject) => {
-                    this.player.getStatus( ({result}) =>{
-                        resolve( Math.round( result.time / result.duration * 100 ) );
-                    });
-                });
-            }
+            getter: () => (this._status.time / this._status.duration) * 100
         } );
         return this;
     }
@@ -121,51 +116,18 @@ class Controller {
      * @function addTrackLabels
      * @description Adds track information labels as shortcuts. Album, Artist and Title.
      */
-    addTrackLabels( {albumLabel,artistLabel,titleLabel} ){
+    addTrackLabels({ albumLabel, artistLabel, titleLabel }){
         this.device
-            .addTextLabel( 
-                { name: 'artistname', label: artistLabel||'Artist' }, 
-                () => new Promise( ( resolve, reject) => this.player.getArtist( ( {result} ) =>{
-                    this._cache.artist = result;
-                    resolve( result );
-                }  ) ) )
-            .addTextLabel( 
-                { name: 'albumname', label: albumLabel||'Album' }, 
-                () => new Promise( ( resolve, reject) => this.player.getAlbum( ( {result} ) => {
-                    this._cache.album = result;
-                    resolve( result );
-                 } ) ) )
-            .addTextLabel( 
-                { name: 'titlename', label: titleLabel||'Title' }, 
-                () => new Promise( ( resolve, reject) => this.player.getTitle( ( {result} ) => {
-                    this._cache.title = result;
-                    resolve( result );
-                 } ) ) )
+            .addTextLabel({ name: 'artistname', label: artistLabel||'Artist' }, () => this._cache.artist )
+            .addTextLabel({ name: 'albumname', label: albumLabel||'Album' }, () => this._cache.album )
+            .addTextLabel({ name: 'titlename', label: titleLabel||'Title' }, () => this._cache.title );
 
         return this;
     }
 
-    updateCacheFromMeta(){
-        return new Promise( (resolve, reject) => {
-            this.player.getCurrentRemoteMeta( ({result}) => {
-                    this._cache.artist = result.artist;
-                    this._cache.album = result.album;
-                    this._cache.title = result.title;
-                    this._cache.cover = this.player.address + ":"+ this.player.port + result.artwork_url;
-                resolve(this._cache.cover );
-            } );
-        } );
-    }
-
     addCurrentTrackCover(){
-        this.device.addImageUrl(
-            { name: 'albumcover', label: 'Cover for current album', size: 'large'}, 
-            ( deviceId ) => new Promise( (resolve, reject) => {
-                this.player.getCurrentRemoteMeta( ({result}) => {
-                    this._cache.cover = this.player.address + ":"+ this.player.port + result.artwork_url;
-                    resolve(this._cache.cover );
-                } );
-            } ) );
+        this.device.addImageUrl({ name: 'albumcover', size: 'large'}, ( deviceId ) => this._cache.cover );
+        return this;
     }
 
     /**
@@ -193,7 +155,7 @@ class Controller {
             if (optionalCallbackFunctions && optionalCallbackFunctions.powerOffNotificationFunction) {
                 this.markDeviceOff = optionalCallbackFunctions.powerOffNotificationFunction;
             }
-            this.updateState();
+            setInterval( this.update.bind(this), this._settings.pollingDelay * 1000 );
         };
 
         this.device.registerSubscriptionFunction( setUpdateCallbackReference.bind(this) );
@@ -214,79 +176,94 @@ class Controller {
         return this._mappings[commandName];
     }
 
-    updateState(){
-        const deviceId = 'default';
-        this.player.getStatus( ({result}) => {
-            // Powet state management
-            if( result.power === 1 ){
-                if(this.markDeviceOn){ 
-                    // console.log(`Invoking markDeviceOn for ${deviceId}`)
-                    this.markDeviceOn(deviceId);
-                }
-            }
-            else {
-                if(this.markDeviceOff) {
-                    // console.log(`Invoking markDeviceOff for ${deviceId}`)
-                    this.markDeviceOff(deviceId);
-                }
-            }
+    /**
+     * This is called internaly to update the state of the driver from the player state.
+     */
+    update(){
+        this.updateStatus().then( this.updateStatusComponents.bind( this ) );
+        this.updateRemoteMeta().then( this.updateRemoteMetaComponents.bind( this ) );
+    }
 
-            // Stops the interval increment by one second for the slider update.
-            // It will be restart below if needed.
-            if( this.durationInterval ){
-                clearInterval( this.durationInterval );
-                this.durationInterval = null;
-            }                 
-
-            if( result.mode == 'play'){
-                // Slider duration management
-                let duration = Math.round( result.time / result.duration * 100 );
-                let durationIncrement = (1 / result.duration) * 100
-                
-                this.durationInterval = setInterval( () => {
-                    // console.log(`Invoking updateCallback for ${deviceId}`)
-                    this.sendComponentUpdate({
-                        uniqueDeviceId: deviceId,
-                        component: 'duration',
-                        value: duration
-                    });
-                    duration += durationIncrement;
-                }, 1 * 1000 );
-
-                // Text label management
-                if( this._cache.artist ){
-                    this.sendComponentUpdate({
-                        uniqueDeviceId: deviceId,
-                        component: 'artistname',
-                        value: this._cache.artist
-                    } );
-                }
-                if( this._cache.album ){
-                    this.sendComponentUpdate({
-                        uniqueDeviceId: deviceId,
-                        component: 'albumname',
-                        value: this._cache.album
-                    } );
-                }
-                if( this._cache.title ){
-                    this.sendComponentUpdate({
-                        uniqueDeviceId: deviceId,
-                        component: 'titlename',
-                        value: this._cache.title
-                    } );
-                }
-                if( this._cache.cover ){
-                    this.sendComponentUpdate({
-                        uniqueDeviceId: deviceId,
-                        component: 'albumcover',
-                        value: this._cache.cover
-                    } );
-                }
-            } 
-            setTimeout( this.updateState.bind(this), this._settings.pollingDelay );
+    /**
+     * Updates the player status
+     */
+    updateStatus(){
+        return new Promise( (resolve, reject) => {
+            this.player.getStatus( ({result}) => {
+                this._status = result;
+                resoleve( this._status );
+            });
         });
     }
 
+    /**
+     * Updates the internal cache of player remote meta.
+     */
+    updateRemoteMeta(){
+        return new Promise( (resolve, reject) => {
+            this.player.getCurrentRemoteMeta( ({result}) => {
+                    this._cache = result;
+                    this._cache.cover = this.player.address + ":"+ this.player.port + result.artwork_url;
+                resolve( this._cache );
+            } );
+        } );
+    }
+
+    /**
+     * Updates power mode and sliders
+     */
+    updateStatusComponents(){
+        const uniqueDeviceId = 'default'; //TODO: gets the device id 
+
+        // Powet state management
+        if( this._status.power === 1 ){
+            if(this.markDeviceOn){ 
+                this.markDeviceOn(uniqueDeviceId);
+            }
+        }
+        else {
+            if(this.markDeviceOff) {
+                this.markDeviceOff(uniqueDeviceId);
+            }
+        }
+
+        // Stops the interval increment by one second for the slider update.
+        // It will be restarted below if needed.
+        if( this.durationInterval ){
+            clearInterval( this.durationInterval );
+            this.durationInterval = null;
+        }                 
+
+        if( this._status.mode == 'play' ){
+            if( this.sendComponentUpdate ) {
+                // Slider duration management
+                let value = Math.round( this._status.time / this._status.duration * 100 );
+
+                if( this._settings.pollingDelay >= 2 ) {
+                    const durationIncrement = (1 / this._status.duration) * 100
+                    this.durationInterval = setInterval( () => {
+                        this.sendComponentUpdate({ uniqueDeviceId, component: 'duration', value });
+                        value += durationIncrement;
+                    }, 1 * 1000 );
+                } else {
+                    // If the polling is every seconds, updates the slider directly...
+                    this.sendComponentUpdate({ uniqueDeviceId, component: 'duration', value });
+                }
+            }
+        } 
+    }
+
+    updateRemoteMetaComponents() {
+        const uniqueDeviceId = 'default'; //TODO: gets the device id 
+        
+        if(this.sendComponentUpdate) {
+            // Text label management
+            this.sendComponentUpdate({ uniqueDeviceId, component: 'artistname', value: this._cache.artist || "" });
+            this.sendComponentUpdate({ uniqueDeviceId, component: 'albumname', value: this._cache.album || "" });
+            this.sendComponentUpdate({ uniqueDeviceId, component: 'titlename', value: this._cache.title || "" });
+            this.sendComponentUpdate({ uniqueDeviceId, component: 'albumcover', value: this._cache.cover || "" });
+        }
+    }
 }
 
 module.exports.build = function( device, player ){
